ca0298c4b78bdfa61f963bd4349a8fabbdf72ac5
mJehanno <mathob.jehanno@hotmail.fr>
2019-07-23 11:34:53 +0200

    feat: Project Init, adding ng zorro

--- apps/cr-tips/src/app/app.component.ts
+++ apps/cr-tips/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'cr-tips-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cr-tips';
+  isCollapsed = false;
 }
