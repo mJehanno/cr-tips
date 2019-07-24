1bce8041481a1d319db7080f524357af5774af9b
mJehanno <mathob.jehanno@hotmail.fr>
2019-07-24 15:06:33 +0200

    chore: From Scss to less

--- apps/cr-tips/src/app/app.component.ts
+++ apps/cr-tips/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'cr-tips-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
   isCollapsed = false;
